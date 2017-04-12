23b01a6c340ecad9975f0ff050bdc32ac2445853
markdev812 <mahogland@gmail.com>
2017-04-12 11:34:08 -0500

    added numSegs param

--- Program.cs
+++ Program.cs
@@ -7,11 +7,34 @@ namespace FileVer2Txt
     {
         static void Main(string[] args)
         {
+            //Usage:  FileVer2Txt [file] [numsegs]
+            //        file = exe or dll assembly file to read
+            //        numsegs = number of version segments to include
+            //                  1 = Major
+            //                  2 = Major.Minor
+            //                  3 = Major.Minor.Build
+            //                  4 = Major.Minor.Build.Rev
             if (args.Length < 1)
                 return;
+            int numSegs = 4;
+            if (args.Length > 1)
+            {
+                int tempSegs;
+                if (int.TryParse(args[1], out tempSegs))
+                    numSegs = tempSegs;
+            }
+
             var assFile = args[0];
             FileVersionInfo fvi = FileVersionInfo.GetVersionInfo(assFile);
-            File.WriteAllText("version.txt", fvi.FileVersion);
+
+            string strFileVer = "";
+            string[] segs = fvi.FileVersion.Split('.');
+            for (int i = 0; i < (segs.Length < numSegs ? segs.Length : numSegs); i++)
+            {
+                if (i > 0) strFileVer += ".";
+                strFileVer += segs[i];
+            }
+            File.WriteAllText("version.txt", strFileVer);
         }
     }
 }
